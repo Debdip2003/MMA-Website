98b962c03ea5db435fa66399c1ab93f83c3c426d
Debdip Bhattacharya <dbhattacharya1912@gmail.com>
2024-07-26 23:16:11 +0530

    firebase added

--- src/components/ContactSection.jsx
+++ src/components/ContactSection.jsx
@@ -1,6 +1,59 @@
-import React from "react";
+import { addDoc, collection } from "firebase/firestore";
+import React, { useState } from "react";
+import { db } from "../utils/firebase";
 
 const ContactSection = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [comment, setComment] = useState("");
+  const [isSubmit, setisSubmit] = useState(false);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const isEmailValid =
+      /^([a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,})$/.test(email);
+
+    const isPhoneNumberValid = /^\d{10}$/.test(phoneNumber);
+
+    if (!name) return alert("Please Enter your Name");
+
+    if (!isPhoneNumberValid) return alert("Please Enter a Proper Phone Number");
+
+    if (!isEmailValid) return alert("Please Enter a Proper Email");
+
+    if (!comment) return alert("Please Enter your message");
+
+    console.log("message sent");
+
+    writeUserData(name, email, phoneNumber, comment);
+  };
+
+  async function writeUserData(name, email, phoneNumber, comment) {
+    setisSubmit(true);
+    try {
+      const docRef = await addDoc(collection(db, "contacts"), {
+        name: name,
+        email: email,
+        phoneNumber: phoneNumber,
+        comment: comment,
+      });
+
+      console.log("Document written with ID: ", docRef.id);
+      alert("Message has been sent!");
+      setName("");
+      setEmail("");
+      setPhoneNumber("");
+      setComment("");
+      setisSubmit(false);
+    } catch (error) {
+      console.error("Error adding document: ", error);
+      alert(error.message);
+      setisSubmit(false);
+    }
+  }
+
 
   return (
     <div className="h-screen w-full bg-gradient-to-b from-black via-black to-gray-800 flex ">
@@ -22,24 +75,35 @@ const ContactSection = () => {
           type="text"
           placeholder="Enter your Full Name"
           className="rounded-2xl w-2/3 p-3 bg-transparent border-[1px] border-white text-white "
+          onChange={(e) => setName(e.target.value)}
+          value={name}
         ></input>
         <input
           type="number"
           placeholder="Enter your Phone Number"
           className="rounded-2xl w-2/3 p-3 bg-transparent border-[1px] border-white  text-white"
+          onChange={(e) => setPhoneNumber(e.target.value)}
+          value={phoneNumber}
         ></input>
         <input
           type="email"
           placeholder="Enter your Email"
           className="rounded-2xl w-2/3 p-3 bg-transparent border-[1px] border-white  text-white"
+          onChange={(e)=>setEmail(e.target.value)}
+          value={email}
         ></input>
         <textarea
           type="text"
           placeholder="Write down some comments"
           className="rounded-2xl w-2/3 p-3 bg-transparent border-[1px] border-white  text-white"
           rows={10}
+          onChange={(e)=>setComment(e.target.value)}
+          value={comment}
         ></textarea>
-        <button className="bg-blue-600 p-2 w-1/5 text-xl hover:cursor-pointer hover:bg-blue-900 duration-200 rounded-2xl text-white">
+        <button
+          className="bg-blue-600 p-2 w-1/5 text-xl hover:cursor-pointer hover:bg-blue-900 duration-200 rounded-2xl text-white"
+          onClick={handleSubmit}
+        >
           Submit
         </button>
       </div>
