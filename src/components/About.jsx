b2b88acd278df383b4dbef90f44cfb1be3327041
Debdip Bhattacharya <dbhattacharya1912@gmail.com>
2024-07-27 22:57:27 +0530

    about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,28 +1,37 @@
-import React from 'react'
-import AboutImageSection from './AboutImageSection'
+import React from "react";
+import AboutImageSection from "./AboutImageSection";
 
 const About = () => {
-    return (
-        <div>
-             <div className='h-screen w-full bg-gradient-to-b from-gray-800 via-black to-black'>
-            <div className='mx-10 flex flex-col items-center justify-between h-full px-10 md:flex-row py-20'>
-             
-              <AboutImageSection/>
-        <div className='flex flex-col justify-center h-full mr-10'>
-                    <h2 className=' text-7xl font-semibold text-white max-w-lg '>About</h2>
-                    <p className='text-gray-500 py-4 max-w-lg'>Next Level MMA is a Combat Sports oriented Trust established in 2015 with a clear vision: to make Combat Sports available to the masses. Our journey began with the establishment of a premier Combat Sports training center in Saltlake, where we provide top-notch facilities and training for athletes of all levels.
-
-Over the years, we have successfully organized a variety of events such as K-PRO, Elblow, and Grind, each designed to showcase talent, foster competition, and build a strong community around Combat Sports. Our commitment goes beyond just events and training; we aim to create a comprehensive ecosystem that supports the growth and development of Combat Sports Athletes, Teachers, Clubs, and associated entities.
-
-At Next Level MMA, we believe in the power of sports to transform lives, build character, and unite communities. Our efforts are directed towards not only nurturing talent but also ensuring that the benefits of Combat Sports are accessible to everyone. Join us in our mission to bring Combat Sports to the next level.
-                    </p>
-                </div>
-            </div>
-      
-
+  return (
+    <div>
+      <div className="h-screen w-full bg-gradient-to-b from-gray-800 via-black to-black">
+        <div className="mx-10 flex flex-col items-center justify-between h-full px-10 md:flex-row py-20">
+          <AboutImageSection />
+          <div className="flex flex-col justify-center h-full mr-10">
+            <h2 className=" text-7xl font-semibold text-white max-w-lg ">
+              About
+            </h2>
+            <p className="text-gray-500 py-4 max-w-lg">
+              Next Level MMA is a Non-Profit Trust registered under West Bengal
+              Registration Rules 1962, with a visionary goal to establish
+              Kolkata as India’s premier Combat Sports capital. We are committed
+              to achieving this by setting up cutting-edge martial arts training
+              centers across the region, organizing a diverse range of
+              high-profile combat sports events throughout the year, and
+              developing a Combat Sports University and Center of Excellence to
+              foster talent and innovation. Our long-term ambitions include
+              creating a Combat Sports equipment manufacturing unit to provide
+              top-quality gear and hosting an annual Awards Night to celebrate
+              and recognize exceptional individuals in the field. Through these
+              comprehensive efforts, we aim to enhance the standards of combat
+              sports, inspire a new generation of athletes, and bring both
+              national and international recognition to Bengal and its people.
+            </p>
+          </div>
         </div>
-        </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default About
+export default About;
--- src/components/AboutUsSection.jsx
+++ src/components/AboutUsSection.jsx
@@ -1,29 +1,40 @@
-import React from 'react'
-import aboutpageImage5 from "..//assests/aboutpageImage5.jpg"
+import React from "react";
+import aboutpageImage4 from "..//assests/aboutpageImage4.jpg";
 
 const AboutUsSection = () => {
-    return (
-        <div>
-            <div className='h-screen w-full bg-gradient-to-b from-black via-black to-gray-800'>
-                <div className='mx-auto flex flex-col items-center justify-between h-full px-10 md:flex-row'>
-                    <div className='md:h-full md:w-1/2 flex items-center justify-center'>
-                        <img src={aboutpageImage5} alt='aboutPage' className='rounded-2xl grayscale w-full md:w-[95%] hover:cursor-pointer hover:grayscale-0 hover:scale-105 duration-200' />
-                    </div>
-                    <div className='flex flex-col justify-center h-full pr-20'>
-                        <h2 className=' text-7xl font-semibold text-white max-w-lg '>Who are we?</h2>
-                        <p className='text-gray-500 py-4 max-w-lg'>
-                            Next Level MMA is the first MMA gym in Salt Lake, and it represents a pioneering effort to make MMA accessible to everyone in the area. Despite Salt Lake being filled with numerous modern amenities like malls, supermarkets, multiplexes, institutes, and western-style gyms, it lacked a dedicated MMA training center until we established our gym in 2015.
-
-                            The youth of Salt Lake have shown a keen interest in MMA, inspired by their enthusiasm for UFC and the sport in general. Our goal is to provide them with the opportunity to train in MMA, filling a significant void in the local sports scene.
-
-                            At Next Level MMA, we offer training in various combat disciplines, including Boxing, Kickboxing, and Brazilian Jiu-Jitsu (BJJ). Our comprehensive programs are designed to cater to both beginners and advanced practitioners, helping them to develop their skills and achieve their personal fitness and combat sports goals.
-                            Our mission extends beyond just training individuals; we aim to create an ecosystem that supports the growth of combat sports athletes, teachers, clubs, and related entities. By fostering a vibrant combat sports community, we hope to inspire more people to engage in and appreciate the discipline, dedication, and benefits of MMA.
-                        </p>
-                    </div>
-                </div>
-            </div>
+  return (
+    <div>
+      <div className="h-screen w-full bg-gradient-to-b from-black via-black to-gray-800">
+        <div className="mx-auto flex flex-col items-center justify-between h-full px-10 md:flex-row">
+          <div className="md:h-full md:w-1/2 flex items-center justify-center">
+            <img
+              src={aboutpageImage4}
+              alt="aboutPage"
+              className="rounded-2xl grayscale w-full md:w-[95%] hover:cursor-pointer hover:grayscale-0 hover:scale-105 duration-200"
+            />
+          </div>
+          <div className="flex flex-col justify-center h-full pr-20">
+            <h2 className=" text-7xl font-semibold text-white max-w-lg ">
+              Who are we?
+            </h2>
+            <p className="text-gray-500 py-4 max-w-lg">
+              Next Level MMA is a Trust (NPO) created to promote Comat Sports in
+              West Bengal. We are registered under West Bengal Registration
+              Rules 1962. We aim to make Kolkata the Combat Sports capital of
+              India. We plan to achieve this by establishing multiple martial
+              arts training centers, holding multiple Combat Sports events round
+              the year, establishing a Combat Sports University and Center of
+              Excellence. In the long term, we would also like to establish a
+              Combat Sports equipment manufacturing unit. A yearly Awards night
+              is conceptualised to award and reward Combat Sports personalities.
+              We believe all of the above activities can help us achieve our
+              objectives and bring glory to Bengal and its people.
+            </p>
+          </div>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default AboutUsSection
+export default AboutUsSection;
